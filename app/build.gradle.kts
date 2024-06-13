6daee999116f93aa18ceb5f29452cd375888f930
dev-xero <elo48858@gmail.com>
2024-06-13 19:59:44 +0100

    build(libs): Add splash screen api lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ android {
 }
 
 dependencies {
+	val splashVersion = "1.2.0-alpha01"
 
 	implementation(libs.androidx.core.ktx)
 	implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -66,4 +67,7 @@ dependencies {
 	androidTestImplementation(libs.androidx.ui.test.junit4)
 	debugImplementation(libs.androidx.ui.tooling)
 	debugImplementation(libs.androidx.ui.test.manifest)
+
+	// Splash Screen API
+	implementation(libs.androidx.core.splashscreen)
 }
